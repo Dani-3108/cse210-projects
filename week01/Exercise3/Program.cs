08eb17a26a5f953bed0bfbd7ef7ed9f0b145ac3c
Dani-3108 <floresd477@gmail.com>
2026-01-10 22:42:24 -0600

    wrote code for week 01 activity 3, fulfiled core requirements and stretch challenge

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        String keepPlaying="yes";
+        while (keepPlaying=="yes")
+        {
+            //guess count variable
+            int guessCount=1;
+            //Random number generator
+            Random randomGenerator=new Random();
+            int magicNumber=randomGenerator.Next(1,100);
+            //core requirement but i dont use since i generate the number randon
+            //Console.Write("What is the magic number? ");
+            //String userInput=Console.ReadLine();
+            //int magicNumber= int.Parse(userInput);
+            //User Input
+            Console.Write("What is your guess? ");
+            String userInput=Console.ReadLine();
+            int userNumber=int.Parse(userInput);
+            //While loop
+            while (userNumber != magicNumber)
+            {
+                //guess count
+                guessCount+=1;
+                // if statements
+                if (userNumber<magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (userNumber>magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                } 
+                //asking again for user input
+                Console.Write("What is your guess? ");
+                userInput=Console.ReadLine();
+                userNumber=int.Parse(userInput);
+            }
+            Console.WriteLine("You guessed the Magic Number!");
+            Console.WriteLine($"It took you {guessCount} guesses to find the magic number.");
+            Console.Write("Do you want to keep playing? ");
+            keepPlaying=Console.ReadLine();
+        }
     }
 }
