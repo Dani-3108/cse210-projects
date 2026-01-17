58b13095ce9123ed761d9ba0e2cebc3bfc062427
Dani-3108 <floresd477@gmail.com>
2026-01-17 16:47:49 -0600

    completed W02 Learning Activity: Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Spoton";
+        job1._jobTitle = "Business Developer Representative";
+        job1._startYear = 2022;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._company = "UnitX";
+        job2._jobTitle = "Software Engineer";
+        job2._startYear = 2025;
+        job2._endYear = 2050;
+
+        Resume myresume = new Resume();
+        myresume._name = "Daniel Osorio";
+
+        myresume._jobs.Add(job1);
+        myresume._jobs.Add(job2);
+
+        myresume.Display();
     }
 }
